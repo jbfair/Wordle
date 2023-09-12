94f08b83547f32f50ecf7be8023d307bd6527110
Jeremy Fairbanks <jf672@byu.edu>
2023-09-11 23:02:17 -0600

    init

---
+++ WordleStarter/Wordle.py
@@ -0,0 +1,34 @@
+# File: Wordle.py
+
+"""
+This module is the starter file for the Wordle assignment.
+BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
+"""
+
+import random
+
+from WordleDictionary import FIVE_LETTER_WORDS
+from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+from WordleGraphics import  WordleSquare
+
+def wordle():
+
+    def enter_action(s):
+        gw.show_message("You have to implement this method.")
+
+    gw = WordleGWindow()
+    gw.add_enter_listener(enter_action)
+
+    #Select Word
+    x = random.randint(0,len(FIVE_LETTER_WORDS))
+    word = FIVE_LETTER_WORDS[x]
+    
+    #Convert word to letters and place in first row
+    for x in range(0,5):
+        gw.set_square_letter(0,x,word[x])
+    
+
+# Startup code
+
+if __name__ == "__main__":
+    wordle()
