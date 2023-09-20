94f7031dbddbfd0e244c88be2ed8a61848b5deb4
Jeremy Fairbanks <jf672@byu.edu>
2023-09-20 17:26:59 -0600

    dup function

--- WordleStarter/Wordle.py
+++ WordleStarter/Wordle.py
@@ -46,6 +46,7 @@ def wordle():
         for l in range(N_COLS):
             guess = guess + gw.get_square_letter(row,l)
         valid, correct = check_word(guess,valid,gameWord,correct)
+        color_letters(guess)
         
     
         if valid == True:    
@@ -54,7 +55,19 @@ def wordle():
         row = gw.set_current_row(row)
 
         
-
+    def color_letters(guess):
+        alphabet = {
+        'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0,
+        'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0,
+        'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0
+        }
+        for l in guess:
+            alphabet[l.lower()] += 1
+        # if 3 in alphabet.values():
+        #     #do something
+        # elif 2 in alphabet.values():
+        #     #do something
+            
         
 
   
